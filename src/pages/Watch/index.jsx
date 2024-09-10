d307e5d80852733af243096ad59aadfefdcc74a0
JoaoSilva2004 <joao.vitor.silva2004@outlook.com>
2024-09-10 10:34:05 -0300

    feat: routes configuration

---
+++ src/pages/Watch/index.jsx
@@ -0,0 +1,24 @@
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+import styles from "./Watch.module.css";
+
+function Watch() {
+
+  return(
+    <>
+      <Header />
+      
+      <Container>
+        <section className={styles.watch}>
+          <h1>Assistir</h1>
+        </section>
+      </Container>
+
+      <Footer />
+    </>
+  )
+}
+
+export default Watch;
