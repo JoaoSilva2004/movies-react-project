c3c71b2930843aa78f1d2b0f1eadb3b7eabbd4c0
JoaoSilva2004 <joao.vitor.silva2004@outlook.com>
2024-09-10 00:46:14 -0300

    feat: aula 05

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Banner from "./components/Banner";
+import Card from "./components/Card";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import videos from "./json/db.json";
 
 function App() {
 
@@ -11,8 +13,15 @@ function App() {
       <Banner image="home" />
 
       <Container>
-        <h1>Hello world!</h1>
-        <p>Olá mundo! Estou aprendendo React JS!</p>
+        <h2>Animes</h2>
+        <section className="cards">
+
+          { videos.map( (video) => <Card id={video.id} key={videos.id} /> ) }
+
+          {/* <Card id="MMKr4T1picE" />
+          <Card id="8ahHh1rVc2w" />
+          <Card id="DojfG9WAcx0" /> */}
+        </section>
       </Container>
 
       <Footer />
