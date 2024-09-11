8d5237d0783c885f712547088c9f1e663996ad30
JoaoSilva2004 <joao.vitor.silva2004@outlook.com>
2024-09-11 13:14:41 -0300

    feat: aula 13

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -2,7 +2,7 @@ import styles from "./Search.module.css";
 import Container from "../../components/Container";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import VideoList from "../../components/VideoList";
+import SearchVideoList from "../../components/SearchVideoList";
 import videos from "../../json/videos.json";
 
 function Search() {
@@ -14,7 +14,8 @@ function Search() {
       <Container>
         <section className={styles.search} >
           <h2>Pesquisar</h2>
-          <VideoList videos={videos} />
+
+          <SearchVideoList videos={videos} />
         </section>
       </Container>
 
